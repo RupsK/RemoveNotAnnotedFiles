4be8e4eea1b4b20a96f49c9317cabb170d8546ca
Rupali Shinde <31463957+RupsK@users.noreply.github.com>
2024-05-22 14:58:05 +0900

    1st upload
    helpful for DB curation

---
+++ remove empy text file.py
@@ -0,0 +1,64 @@
+
+
+
+
+
+from PIL import Image
+import os
+
+
+
+import os
+import glob
+i = 0
+j = 0
+for img in glob.glob("C:/Users/h4tec/Desktop/CBCopy/Cropped dataset KT/*.jpg"):
+	
+    image= Image.open(img)
+    
+    s = os.path.basename(img)
+    name =s.replace('.jpg', '')
+    
+    image_width, image_height = image.size
+    
+    text_file_path = "C:/Users/h4tec/Desktop/sample/" + name + ".txt"  #C:Users/h4tec/Downloads/1767image/obj_train_data/" + name + ".txt
+    if os.path.exists(text_file_path):
+           with open(text_file_path, 'r') as file:
+                   check_file = os.stat(text_file_path).st_size
+                   
+                   if check_file > 0:
+                           print("The file is not empty.")
+                   else:
+                           
+                           print(" empty") 
+                           file.close()
+                           image.close()
+                           os.remove(text_file_path)
+                           os.remove(img)
+    else:
+           
+           print("file does not exist")
+          
+           image.close()
+           os.remove(img)
+
+"""
+
+	
+    if os.path.exists(text_file_path):
+    	with open(text_file_path, 'w') as file:
+                    check_file = os.stat(text_file_path).st_size
+                    if (check_file == 0):
+                            print("The file is empty.")
+                            file.close()
+                            image.close()
+                            os.remove(text_file_path)
+                            os.remove(img)
+                    else:
+                            print("not empty")
+    else:
+            print("file does not exist")
+            image.close()
+            os.remove(img)
+           
+"""
